47e64a3691dccbc1a3e93da8506b1fb8d1eddcf6
HyeChang <92506172+HyeChang@users.noreply.github.com>
2023-06-14 23:37:06 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        
+        String userID;
+        Scanner sc = new Scanner(System.in);
+        
+        userID = sc.nextLine();
+        
+        System.out.println( userID + "??!" );
+        
+    }
+}
