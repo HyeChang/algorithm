c763648894852ead9499f5668479d644689bff80
HyeChang <92506172+HyeChang@users.noreply.github.com>
2023-06-14 23:36:41 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17916 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        
+        double A, B;
+        Scanner sc = new Scanner(System.in);
+        
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+        
+        System.out.println( (A/B) );
+        
+    }
+}
