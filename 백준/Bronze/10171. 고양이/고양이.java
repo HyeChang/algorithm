0a2a756a73762eff66c9779cd0adc6e98cb09ca7
HyeChang <92506172+HyeChang@users.noreply.github.com>
2023-06-14 23:37:36 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+class Main {
+	
+    public static void main(String[] args) throws Exception {
+
+    	System.out.println("\\    /\\");
+    	System.out.println(" )  ( ')");
+    	System.out.println("(  /  ) ");
+    	System.out.println(" \\(__)| ");
+		
+    }
+}
