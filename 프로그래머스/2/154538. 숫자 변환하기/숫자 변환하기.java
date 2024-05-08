96f0c9d65aa4b43d1af94d6e58e3ef78e4e41cf7
HyeChang <92506172+HyeChang@users.noreply.github.com>
2024-05-09 00:13:00 +0900

    [level 2] Title: 숫자 변환하기, Time: 2.33 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,47 +1,49 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int x, int y, int n) {
-        
+
         int answer = 0;
+
         Queue<Integer> que = new LinkedList<>();
         Set<Integer> visited = new HashSet<>();
-        
+
         que.add(x);
         visited.add(x);
         // bfs
         while(!que.isEmpty()) {
-            
+
             int size = que.size();
-            
+
+            // 연산 횟수
             for(int i = 0; i < size; i++) {
-                
-                int cursor = que.poll();
-            
-                if(cursor == y) {
-                    return answer;
+
+                int cur = que.poll();
+
+                // y 찾을 시 바로 종료(최소)
+                if(cur == y) return answer;
+
+                // 조건 만족 시 다음 연산 수행을 위해 큐에 값 저장
+                if( (cur + n) <= y && !visited.contains(cur + n) ) {
+                    que.add(cur + n);
+                    visited.add(cur + n);
+                }
+                if( (cur * 2) <= y && !visited.contains(cur * 2) ) {
+                    que.add(cur * 2);
+                    visited.add(cur * 2);
+                }
+                if( (cur * 3) <= y && !visited.contains(cur * 3) ) {
+                    que.add(cur * 3);
+                    visited.add(cur * 3);
                 }
 
-                if((cursor + n) <= y && !visited.contains(cursor + n)) {
-                    que.add(cursor + n);
-                    visited.add(cursor + n);
-                }
-                if((cursor * 2) <= y && !visited.contains(cursor * 2)) {
-                    que.add(cursor * 2);
-                    visited.add(cursor * 2);
-                }
-                if((cursor * 3) <= y && !visited.contains(cursor * 3)) {
-                    que.add(cursor * 3);
-                    visited.add(cursor * 3);
-                }
-                
             }
+            // 연산 횟수 증가
             answer++;
-        } // while
-        
+
+        }
+
         return -1;
-        
+
     }
-    
 }
